a33fa7ce3db3428975bd28fff487501034dc1d36
Katherine Wu <kwu@newrelic.com>
2017-03-30 12:42:42 -0700

    List out given examples of valid range request headers

--- spec/endpoints/paginator_spec.rb
+++ spec/endpoints/paginator_spec.rb
@@ -25,4 +25,33 @@ describe Pagination do
 
     expect(parse_range_request_header(request_header)).to be == expected
   end
+
+  it "should parse range request header with start identifier" do
+    # Range: id 1..
+  end
+
+  it "should parse range request header with numeric start and end identifiers" do
+    # Range: id 1..5
+  end
+
+  it "should parse range request header with nonnumeric start and end identifiers" do
+    # Range: name ]my-app-001..my-app-999
+  end
+
+  it "should parse range request header with exclusivity operator" do
+    # Range: id ]5..
+  end
+
+  it "should parse range request header with page size" do
+    # Range: id 1..; max=5
+  end
+
+  it "should parse range request header with ordering" do
+    # Range: id 1..; order=desc
+  end
+
+  it "should parse range request header with all elements" do
+    # Range: id ]5..10; max=5, order=desc
+    # Range: name ]my-app-001..my-app-999; max=10, order=asc
+  end
 end
